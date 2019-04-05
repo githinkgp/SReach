70d3d1bbd24c49d6736a0477c741457bb3ecf9dd
githinkgp <githinkgp@gmail.com>
2019-04-05 11:20:53 -0400

    Add files via upload

---
+++ SReach.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import matplotlib.pyplot as plt
+N=6
+
+for n in range(1,7)
+    for q in range(1,2)
+        for theta in 
+x=[1,2,3]
+y=[2,4,1]
+
+plt.plot(x,y)
+
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+
+plt.show()
+print "hello"
+
